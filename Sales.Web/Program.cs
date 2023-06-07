cf623ee11bbf238f0c1192e05e69f5a8e489cd64
mcarvajal <carmalino@gmail.com>
2023-06-07 08:45:39 -0500

    Country Crud

--- Sales.Web/Program.cs
+++ Sales.Web/Program.cs
@@ -1,3 +1,4 @@
+using CurrieTechnologies.Razor.SweetAlert2;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Sales.Web;
@@ -15,5 +16,7 @@ builder.Services.AddHttpClient<IRepository, Repository>(client =>
 {
     client.BaseAddress = new Uri("https://localhost:7011/");
 });
+
+builder.Services.AddSweetAlert2();
 
 await builder.Build().RunAsync();
